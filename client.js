0fba1a638ada07f2294bc7a031896842a16a115e
crowndev22 <crown.fx@proton.me>
2024-05-18 01:50:51 -0500

    manual hydration ig

--- client.js
+++ client.js
@@ -1,8 +1,4 @@
-const { readFileSync } = require("fs");
-const { createServer } = require("http");
-const { parse } = require("url");
-const { renderToString } = require("react-dom/server");
-const React = require("react");
+ReactDOM.hydrateRoot(document.getElementById("root"), <Home />);
 
 const pizzas = [
   {
@@ -66,24 +62,3 @@ function MenuItem({ pizza }) {
     </li>
   );
 }
-
-const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
-const clientJS = readFileSync(`${__dirname}/client.js`, "utf-8");
-
-const server = createServer((req, res) => {
-  const pathName = parse(req.url, true).pathname;
-
-  if (pathName === "/") {
-    const renderedReact = renderToString(<Home />);
-    const html = htmlTemplate.replace("###CONTENT###", renderedReact);
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(html);
-  } else if (pathName === "/client.js") {
-    res.writeHead(200, { "Content-Type": "application/javascript" });
-    res.end(clientJS);
-  } else res.end("The URL cannot be found.");
-});
-
-server.listen(8000, () =>
-  console.log("Listening for requests on port 8000...")
-);
